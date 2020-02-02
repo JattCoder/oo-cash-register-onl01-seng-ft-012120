8b0f2fce3ec6c0c2b2eb6104eaa84d704ed9a9e5
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-02 03:32:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-    @quantity
+    @quantity = quantity
     @price = price
     @total += price * quantity
     if quantity > 1
